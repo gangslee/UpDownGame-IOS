7efe5dd6fb13f26a4f7e35bc1841e89cda26e364
gangslee <grandnet1225@gmail.com>
2021-01-31 14:40:59 +0900

    Set Value Labels with Variables

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -8,13 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var randomValue: Int = 0
+    var tryCount: Int = 0
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"), for: .normal)
+        reset()
     }
 
     @IBAction func sliderValueChanged(_ sender: UISlider){
@@ -27,6 +34,11 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpResetButton(_ sender: UIButton){
         print("touch up reset button")
+        reset()
+    }
+    
+    func reset(){
+        print("reset!")
     }
 }
 
