656c57b65fbac40ed705d1776eeaffc89b60ac62
gangslee <grandnet1225@gmail.com>
2021-02-23 21:18:41 +0900

    Create Credit View

---
+++ UpDownGame/CreditViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by 이경수 on 2021/02/23.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    
+    @IBAction func touchUpDismissButton(_ sender:UIButton){
+        dismiss(animated: true, completion: nil)
+    }
+
+
+}
